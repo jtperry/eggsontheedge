4e6d3f35dfd5f0697513827d0a93e886f7802ab1
JT Perry <pjt@vmware.com>
2024-11-29 21:40:00 -0800

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blahg-bsky.netlify.app/",
+	site: "https://ontheedgefarms.com/",
 	base: "/",
 	integrations: [sitemap()],
 	markdown: {
